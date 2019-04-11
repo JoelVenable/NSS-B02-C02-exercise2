fa45176d95eaab54756377855eefd109ddcdf0b6
Joel Venable <joelmusicman@gmail.com>
2019-04-11 11:56:37 -0500

    Finished first lightning exercise

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const lassie = {
+  age: 7,
+  breed: "Collie",
+  color: "White and Brown"
+}
+
+console.log(`Lassie's breed is ${lassie.breed}`);
+
+console.log(`Lassie's age is ${lassie.age}`);
+
+
+
+//  Output each value to the console using dot notation:
+
+const wardrobe = {
+  height: 80,
+  manufacturer: 'Killibrew & Sons',
+  contents: ['Dress shirt', 'Jeans', 'Suit','Skirt','Tennis shoes'],
+  depth: 38,
+  width: 50
+}
+
+console.log(`height: ${wardrobe.height}`);
+console.log(`manufacturer: ${wardrobe.manufacturer}`);
+console.log(`contents: ${wardrobe.contents}`);
+console.log(`depth: ${wardrobe.depth}`);
+console.log(`width: ${wardrobe.width}`);
+
