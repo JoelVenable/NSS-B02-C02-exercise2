b27f5e2e76724313578041573bd2842d911fabee
Joel Venable <joelmusicman@gmail.com>
2019-04-11 12:55:37 -0500

    Finished lightning exercises

--- js/main.js
+++ js/main.js
@@ -46,3 +46,35 @@ console.log(`key to lookup: ${keyToLookup}`);
 
 const lassiesBreed = lassie[keyToLookup];
 
+
+
+//  Lightning Exercises
+
+const empireStateBuilding = {
+  stories: 103,
+  height: 1453,
+  address: "350 Fifth Avenue, Manhattan, New York 10118",
+  squareFeet: 2768591,
+  constructionDate: 1931,
+  cost: 40948900,
+  owner: "Empire State Realty Trust",
+  eastWestLength: 424,
+  northSouthLength: 187,
+  architect: "Shreve, Lamb & Harmon"
+}
+
+// Use dot notation to output all the dimensions of the Empire State Building to the console
+
+console.log(`Stories: ${empireStateBuilding.stories}`);
+console.log(`height: ${empireStateBuilding.height}`);
+console.log(`square feet: ${empireStateBuilding.squareFeet}`);
+console.log(`East-West Length: ${empireStateBuilding.eastWestLength}`);
+console.log(`North South Length: ${empireStateBuilding.northSouthLength}`);
+
+// Use square bracket notation to output the remaining 5 properties to console.
+
+console.log(`address: ${empireStateBuilding['address']}`);
+console.log(`Construction Date: ${empireStateBuilding['constructionDate']}`);
+console.log(`cost: ${empireStateBuilding['cost']}`);
+console.log(`owner: ${empireStateBuilding['owner']}`);
+console.log(`Architect: ${empireStateBuilding['architect']}`);
